7173c2313db767c993bcc461a43b27ee8fded3e3
kim seol gyu <sgk0418on@naver.com>
2019-02-15 10:31:51 +0900

    api commit

---
+++ js/weather.js
@@ -0,0 +1,39 @@
+const API_KEY ="812c6a379e6a06d994b94d218567f13b";
+const COORDS = "coords";
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError(){
+    console.log("Cant access geo location");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCords = localStorage.getItem(COORDS);
+    if(loadedCords === null){
+        askForCoords();
+    } else {
+        //getWeather
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
